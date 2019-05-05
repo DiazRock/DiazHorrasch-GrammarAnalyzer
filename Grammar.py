e8ff07e064a64915f964d05be5a435b6d29fa811
AlejandroDiaz <alediazroq@gmail.com>
2019-05-05 17:35:08 -0400

    Ya escribí un indizador en la GrammarClass, y creo que esto va a quedar bastante bien :)

--- Grammar.py
+++ Grammar.py
@@ -1,17 +1,28 @@
 class GrammarSymbol:
     def __init__(self, name):
         self.name = name
-
+    def __repr__(self):
+        return self.name
 class Terminal(GrammarSymbol):
     pass
 
 class NoTerminal(GrammarSymbol):
-    def __init__(self, name, productionsBodies):
+    def __init__(self, name, *productionsBodies):
         super().__init__(name)
         self.productionsBodies = productionsBodies
     
-class Grammar:
+class GrammarClass:
     def __init__(self, initialSymbol, terminals, nonTerminals):
         self.terminals = terminals
         self.nonTerminals = nonTerminals
         self.initialSymbol = initialSymbol
+    def __repr__(self):
+        return "S: {0}\nT: {1}\nNT: {2}".format(self.initialSymbol, self.terminals, self.nonTerminals)
+    def __getitem__(self, index):
+        if index is self.initialSymbol : return self.initialSymbol
+        if index in self.nonTerminals : return self.nonTerminals[index]
+        if index in self.terminals : return self.terminals[index]
+        return False
+class ProductionBody:
+    def __init__(self, *grammarSymbols):
+        self.grammarSymbols = grammarSymbols
