5f032f9ea91ce9ed921eede9bb7d5ab510aaf7c5
AlejandroDiaz <alediazroq@gmail.com>
2019-05-13 05:16:31 -0400

    Ya el First funciona

--- AuxiliarMethods.py
+++ AuxiliarMethods.py
@@ -1,27 +1,57 @@
 from Grammar import *
 
 def CalculateFirst(G:GrammarClass):
-    Firsts = {x:x.name for x in G.terminals}
-    Firsts.update({X:{} for X in G.nonTerminals})
+    Firsts = {x:{x.name} for x in G.terminals}
+    Firsts.update({X:set() for X in G.nonTerminals})
+    
     changed = False
-    while(changed or not Firsts[G.initialSymbol]):
+    while(changed or not Firsts[G.initialSymbol]):        
         for X in G.nonTerminals:
-            for prod in X.productionsBodies:
-                if prod == ProductionBody(Epsilon):
-                   changed = Add(Firsts[X], [Epsilon])
+            for prod in X.productionsBodies:                          
+                if prod == [Epsilon()]:
+                   changed = Add(Firsts[X], [Epsilon()])
                 else:
                     allEpsilon = True
-                    for x in prod:                    
+                    for x in prod:
+                        print(X, x, sep = "\t")                        
                         changed = Add(Firsts[X], Firsts[x])
-                        if not Epsilon in Firsts[x]: break
-                    if allEpsilon : changed = Add(Firsts[X], [Epsilon])        
+                        if not Epsilon() in Firsts[x]: 
+                            allEpsilon = False
+                            break
+                    if allEpsilon : changed = Add(Firsts[X], [Epsilon()])        
     return Firsts
-def CalculateFollow(G:GrammarClass, Firsts):
-    Follows = {X:{} for X in G.nonTerminals}
-    Follows[G.initialSymbol].Add(FinalSymbol)
     
+def CalculateFollow(G:GrammarClass, Firsts):
+    Follows = {X:set() for X in G.nonTerminals}
+    Follows[G.initialSymbol] += [FinalSymbol()]
+    changed = False
+    while(changed or Follows[G.initialSymbol] == [FinalSymbol()]):
+        for X in G.nonTerminals:
+            for prod in X.productionsBodies:
+                for i in range(len(prod)):
+                    if prod[i] is NoTerminal:
+                        firstBody = FirstsForBody(prod[i:], Firsts)
+                        changed = Add(Follows[prod[i]], [x for x in firstBody and x != Epsilon()])
+                        if(Epsilon() in firstBody or i == len(prod) - 1):
+                            changed = Add(Follows[prod[i]], Follows[X])
+
     return Follows
+
+def FirstsForBody(productionBody, Firsts):
+    result = set()
+    allEpsilon = True
+    for x in productionBody:
+        result.union (Firsts[x])
+        if Epsilon() not in Firsts[x]:
+            allEpsilon = False
+            break
+    if allEpsilon:
+        result += Epsilon()
+    return result
+
 def Add(First, terminalList):
     before = First.copy()
-    First += terminalList
+    print(First, terminalList, sep = "\t")
+    First.update(terminalList)
+    print(First, before, First != before, sep = "\t")    
     return before != First
--- Grammar.py
+++ Grammar.py
@@ -1,42 +1,50 @@
 class GrammarSymbol:
     def __init__(self, name):
         self.name = name
+    def __hash__(self):
+        return hash (self.name)
     def __repr__(self):
         return self.name
+    def __eq__(self, other):
+        return self.name == other.name
 
 class Terminal(GrammarSymbol):
     pass
 
-class Epsilon(GrammarSymbol):
+class Epsilon(Terminal):
     def __init__(self):
-        super.__init__(self, name="œ")
+        super().__init__(name="œ")
 
-class FinalSymbol(GrammarSymbol):
+class FinalSymbol(Terminal):
     def __init__(self):
-        super.__init__(self, name="$")
+        super().__init__(name="$")
         
 class NoTerminal(GrammarSymbol):
-    def __init__(self, name, *productionsBodies):
+    def __init__(self, name):
         super().__init__(name)
-        self.productionsBodies = productionsBodies
-    
+        self.productionsBodies = list()
+            
 class GrammarClass:
     def __init__(self, initialSymbol, terminals, nonTerminals):
-        self.terminals = terminals
-        self.nonTerminals = nonTerminals
-        self.initialSymbol = initialSymbol
+        self.terminals = [Terminal(x) for x in terminals]
+        self.nonTerminals = [NoTerminal(x) for x in nonTerminals]
+        self.initialSymbol = NoTerminal(name = initialSymbol)
+        
+
     def __repr__(self):
         return "S: {0}\nT: {1}\nNT: {2}".format(self.initialSymbol, self.terminals, self.nonTerminals)
-    def __getitem__(self, index):
-        if index is self.initialSymbol : return self.initialSymbol
-        if index in self.nonTerminals : return self.nonTerminals[index]
-        if index in self.terminals : return self.terminals[index]
-        return False    
-class ProductionBody:
-    def __init__(self, *grammarSymbols):
-        self.grammarSymbols = grammarSymbols
-    def __equal__(self, other):
-        return self.grammarSymbols == other.grammarSymbols
-    def __getitem__(self, index):
-        return self.grammarSymbols[index]
+    def addProduction(self, noTerminal, *productions):
+        index = GrammarClass.__indexOf(self, GrammarSymbol(noTerminal))
+        for production in productions:                                                        
+            self.nonTerminals[index].productionsBodies.append([GrammarSymbol(name = x) for x in production])
+            
+    def __indexOf(self, grammarSymbol):
+        try:
+            return self.nonTerminals.index(grammarSymbol)
+        except ValueError:
+            try:
+                return self.terminals.index(grammarSymbol)
+            except ValueError as e:
+                raise e         
+    
 
